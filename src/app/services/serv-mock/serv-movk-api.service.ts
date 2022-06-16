a012d84fce232c01ff8156d1358f5ed2db60ffde
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-06-16 19:35:41 -0300

    mock config

--- src/app/services/serv-mock/serv-movk-api.service.ts
+++ src/app/services/serv-mock/serv-movk-api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ServMovkApiService {
-  private API_PREFIX = 'http://localhost:3000';
+  private API_PREFIX = 'http://192.168.0.90:3000';
 
   constructor(private http: HttpClient) {}
 
