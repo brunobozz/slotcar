f3377649cdfcc364a110f64724ef37e7e726f313
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-06-28 14:05:45 -0300

    2 players laps records

--- src/app/modules/race/race.component.ts
+++ src/app/modules/race/race.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { ServFunctionsService } from 'src/app/services/serv-functions/serv-functions.service';
 import { ServSocketioService } from 'src/app/services/serv-socketio/serv-socketio.service';
 
 @Component({
@@ -10,12 +11,14 @@ import { ServSocketioService } from 'src/app/services/serv-socketio/serv-socketi
 export class RaceComponent implements OnInit {
   public lapsP1: any = [];
   public lapsP2: any = [];
-  public firstLap = true;
+  public firstLapP1 = true;
+  public firstLapP2 = true;
   public raceStatus = 'reseted';
   public lights: any = [];
 
   constructor(
     private socketIo: ServSocketioService,
+    private functions: ServFunctionsService,
     private toastr: ToastrService
   ) {}
 
@@ -23,30 +26,43 @@ export class RaceComponent implements OnInit {
     this.listenLap();
   }
 
+  // escuta a volta marcada
   private listenLap() {
     this.socketIo.listen().subscribe((res: any) => {
       let theLap = res.split('\r');
       theLap = theLap[0].split('-');
-      if (!this.firstLap) {
-        if (theLap[0] === 'P1') {
-          this.lapsP1.push(theLap[1]);
-        } else if (theLap[0] === 'P2') {
-          this.lapsP2.push(+theLap[1]);
-        }
-      }
-      this.firstLap = false;
+      //chama a gravação da volta
+      this.recordLap(theLap);
     });
+  }
+
+  private recordLap(thisLap: any) {
+    // se for P1
+    if (thisLap[0] === 'P1') {
+      if (!this.firstLapP1) {
+        this.lapsP1.push(thisLap[1]);
+      }
+      this.firstLapP1 = false;
+    }
+    // se for P2
+    if (thisLap[0] === 'P2') {
+      if (!this.firstLapP2) {
+        this.lapsP2.push(thisLap[1]);
+      }
+      this.firstLapP2 = false;
+    }
   }
 
   async startRace() {
     this.raceStatus = 'releasing';
+    // luzes vermelhas
     for (let i = 0; i < 5; i++) {
-      console.log('to no for');
-      await this.delay(1000);
+      await this.functions.delay(1000);
       this.lights.push('r');
     }
+    // luz verde com um tempo sutil randomico
     let randomTime = Math.floor(Math.random() * (6000 - 1000 + 1)) + 1000;
-    await this.delay(randomTime);
+    await this.functions.delay(randomTime);
     this.lights = ['g', 'g', 'g', 'g', 'g'];
     this.raceStatus = 'started';
   }
@@ -57,16 +73,13 @@ export class RaceComponent implements OnInit {
 
   public resetRace() {
     if (confirm('Tem certeza que quer resetar a corrida?') == true) {
-      this.toastr.error('Corrida resetada');
-      this.firstLap = true;
+      this.raceStatus = 'reseted';
+      this.toastr.info('Corrida resetada');
+      this.firstLapP1 = true;
+      this.firstLapP2 = true;
       this.lapsP1 = [];
       this.lapsP2 = [];
       this.lights = [];
-      this.raceStatus = 'reseted';
     }
-  }
-
-  private delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
